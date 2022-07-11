2a6891f095325e7cd617133f04ee35718fbdf00d
alhamzzza <61141101+alhamzzza@users.noreply.github.com>
2022-07-11 03:06:46 +0300

    Update template.js

--- template.js
+++ template.js
@@ -366,6 +366,12 @@ window.onload = function () {
           { id: 76, pid: 65, tags: ["Staff"], name: "حمزة" },
 
           { id: 77, pid: 69, tags: ["Staff"], name: "عمار" },
+           { id: 505, pid: 69, tags: ["Staff"], name: "خالد" },
+
+          { id: 506, pid: 69, tags: ["Staff"], name: "محمد" },
+
+          { id: 507, pid: 69, tags: ["Staff"], name: "عبد الحميد" },
+
           { id: 78, pid: 75, tags: ["Staff"], name: "خالد" },
 
           { id: 79, pid: 66, tags: ["Staff"], name: "فادي" },
